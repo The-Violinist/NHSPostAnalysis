e5208476f72b8a70eb9a432ced7db3760668b581
Kimberly Dills <68087327+kddills@users.noreply.github.com>
2020-11-08 12:28:59 -0800

    Update ssh.py

--- Script/Workup/Kim/ssh.py
+++ Script/Workup/Kim/ssh.py
@@ -3,6 +3,12 @@ import time
 import paramiko
 import smtplib
 
+# import paramiko for SSH network connections which was different than what we used in class with the brute force challenge that used "from pexpect import pxssh" 
+# pexpect didn't work when being ran in windows and I needed to find a different tool to be able to run it in Windows OS , so this is why I went with "import paramiko"
+
+ 
+
+
 def brute():
     filepath=open("./passwordlist.txt")
     ssh=paramiko.SSHClient()
